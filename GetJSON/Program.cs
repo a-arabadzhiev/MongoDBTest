afffe5622bc8cca34edd69054dd43631a642b2f9
Alex Arabadzhiev <a.arabadzhiev@edynamix.com>
2024-08-29 14:23:32 +0100

    тест

---
+++ GetJSON/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+
+namespace JSON_TEST
+{
+    public class GetJSON
+    {
+        //public class JSON
+        //{
+        //    public List<VehicleType>? vehicleTypes { get; set; }
+        //}
+
+        public class VehicleType
+        {
+            public string? name { get; set; }
+        }
+
+        public static void Main()
+        {
+
+            string jsn = """{"name": "Car"}""";
+
+             VehicleType? res = JsonSerializer.Deserialize<VehicleType>(jsn);
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ JSON TEST/GetJSON.cs
@@ -0,0 +1,45 @@
+﻿//using MongoDB.Bson;
+//using MongoDB.Bson.IO;
+//using MongoDB.Driver;
+using System.Text.Json;
+
+namespace JSON_TEST
+{
+    public class GetJSON
+    {
+        public class JSON
+        {
+            public List<VehicleType>? vehicleTypes { get; set; }
+        }
+
+        public class VehicleType
+        {
+            public string? name { get; set; }
+        }
+
+        public static void Main()
+        {
+
+            JSON root = new JSON()
+            {
+                vehicleTypes = new List<VehicleType>(),
+            };
+
+            VehicleType vehicletype = new VehicleType()
+            {
+                name = "CAR",
+            };
+
+            Console.WriteLine(root);
+            Console.ReadKey();
+
+            //string jsn = "{"vehicleTypes": [{"name": "Bike"}, {"name": "Car"}]}"
+
+            //JSON? res = JsonSerializer.Deserialize<JSON>(jsn);
+
+            //Console.WriteLine(res);
+            //Console.ReadKey();
+        }
+    }
+}
+
